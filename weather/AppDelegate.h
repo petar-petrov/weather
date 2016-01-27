14fdfc2faa84b846e12f4856910328cd2c97b7ba
petar-petrov <petarpetrov86@yahoo.com>
2016-01-27 14:07:19 +0200

    Initial Commit

---
+++ weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weather
+//
+//  Created by Petar Petrov on 27/01/2016.
+//  Copyright © 2016 Petar Petrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
